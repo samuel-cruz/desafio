263bf9d033ff020326e91f5ea8798f1e85a45099
Samuel da Cruz <44683435+samuel-cruz@users.noreply.github.com>
2020-09-25 00:16:33 -0300

    Update SecurityConfig.java

--- src/main/java/br/com/meta/config/SecurityConfig.java
+++ src/main/java/br/com/meta/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
 
 /**
  * @author samuel-cruz
@@ -31,7 +32,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.and() //
 				.httpBasic() //
 				.and() //
-				.csrf().disable();
+				.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()) //
+				.and().csrf().disable();
 	}
 
 	@Autowired
